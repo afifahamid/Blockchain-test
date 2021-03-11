b450cc6814cb8165a8e1ca5ce94ca8bd1815f717
Afifa Hamid <50275132+afifahamid@users.noreply.github.com>
2021-03-11 18:41:36 +0500

    Test Codes

---
+++ blockchain.test.js
@@ -0,0 +1,32 @@
+const Blockchain = require('./blockchain');
+
+const Block = require('./block')
+    
+describe('Blockchain', function(){
+let bc, bc2;
+
+        beforeEach(()=> {
+        bc = new Blockchain();
+        bc2 = new Blockchain();
+        });
+
+        it('starts with genesis block', () => {
+        expect(bc.chain[0]).toEqual(Block.genesis());
+        })
+
+        it('add new block', () => {
+        const data = `foo`;
+        bc.addBlock(data);
+        expect(bc.chain[bc.chain.length -1].data).toEqual(data);
+        })
+
+        it('validates a valid chain', ()=>{
+        bc2.addBlock(`foo`);
+        expect(bc.isValidChain(bc2.chain)).toBe(true)
+        })
+
+        it('invalidates a chain with corrupt genesis block', ()=>{
+        bc2.chain[0].data = `wrong information`;
+        expect(bc.isValidChain(bc2.chain)).toBe(false)
+        })
+})
